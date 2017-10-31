31059101455bc46f4541e43847224aa4686862dd
Abhinav Choudhury <achoudh3@ncsu.edu>
2017-10-31 01:02:47 -0400

    Code cleanup, comments and more concise implementation of Heap

--- src/find_political_donors.py
+++ src/find_political_donors.py
@@ -3,84 +3,60 @@ import time
 
 # A heap class that can be initialized to work as a Max-heap or a min-heap
 class Heap(object):
-    def __init__(self, _isMaxHeap=1):
+    def __init__(self, _isMaxHeap=True):
         self.__isMaxHeap = _isMaxHeap
         self.__heap = []
 
     # Checks if 0-based index is a leaf
     def __isLeaf(self, index):
-        __heap_index = index + 1                # Convert to heap indexing
-        __last_non_leaf_index = int(len(self.__heap)/2)
+        __heap_index = index + 1                            # Convert to 1-based heap indexing
+        __last_non_leaf_index = int(len(self.__heap)/2)     # Last non-leaf node is parent of last element
         return(__heap_index > __last_non_leaf_index)
 
-    # Sift up function for Max Heap
-    def __siftUpMax(self, index):
-        __heap_index = index + 1                # Convert to heap indexing
-        if __heap_index != 1:                   # Not root
+    # A comparator function that evaulates x < y if min-heap, or x > y if max-heap
+    def __compare(self, x, y):
+        if self.__isMaxHeap:
+            return x > y
+        else:
+            return x < y
+
+    # Sift up
+    def __siftUp(self, index):
+        __heap_index = index + 1                            # Convert to 1-based heap indexing
+        if __heap_index != 1:                               # Not root
             parent = int(__heap_index/2)
-            parent -= 1                         # Convert to Python list indexing
-            if self.__heap[index] > self.__heap[parent]:
+            parent -= 1                                     # Convert to Python list indexing
+            if self.__compare(self.__heap[index], self.__heap[parent]):
                 self.__heap[index], self.__heap[parent] = self.__heap[parent], self.__heap[index]
-                self.__siftUpMax(parent)
+                self.__siftUp(parent)
 
-    # Sift up function for Min Heap
-    def __siftUpMin(self, index):
-        __heap_index = index + 1                # Convert to heap indexing
-        if __heap_index != 1:                   # Not root
-            parent = int(__heap_index/2)
-            parent -= 1                         # Convert to Python list indexing
-            if self.__heap[index] < self.__heap[parent]:
-                self.__heap[index], self.__heap[parent] = self.__heap[parent], self.__heap[index]
-                self.__siftUpMin(parent)
-
-
+    # Insert a new value into heap
     def insert(self, val):
         self.__heap.append(val)
-        if self.__isMaxHeap:
-            self.__siftUpMax(len(self.__heap)-1)
-        else:
-            self.__siftUpMin(len(self.__heap)-1)
+        self.__siftUp(len(self.__heap)-1)
 
-    # Heapify for max heap
-    def __heapifyMax(self, index):
+    # Heapify
+    def __heapify(self, index):
         if not self.__isLeaf(index):
-            __heap_index = index + 1            # Convert to heap indexing
+            __heap_index = index + 1                        # Convert to 1-based heap indexing
             l = 2*__heap_index
             r = 2*__heap_index + 1
 
-            l -= 1                              # Convert to Python list indexing
-            r -= 1                              # Convert to Python list indexing
+            l -= 1                                          # Convert to Python list indexing
+            r -= 1                                          # Convert to Python list indexing
 
-            largest = index
-            if l < len(self.__heap) and self.__heap[largest] < self.__heap[l]:
-                largest = l
-            if r < len(self.__heap) and self.__heap[largest] < self.__heap[r]:
-                largest = r
+            best = index
+            if l < len(self.__heap) and self.__compare(self.__heap[l], self.__heap[best]):
+                best = l
+            if r < len(self.__heap) and self.__compare(self.__heap[r], self.__heap[best]):
+                best = r
 
-            if largest != index:
-                self.__heap[index], self.__heap[largest] = self.__heap[largest], self.__heap[index]
-                self.__heapifyMax(largest)
+            if best != index:
+                self.__heap[index], self.__heap[best] = self.__heap[best], self.__heap[index]
+                self.__heapify(best)
 
-    # Heapify for min heap
-    def __heapifyMin(self, index):
-        if not self.__isLeaf(index):
-            __heap_index = index + 1            # Convert to heap indexing
-            l = 2*__heap_index
-            r = 2*__heap_index + 1
 
-            l -= 1                              # Convert to Python list indexing
-            r -= 1                              # Convert to Python list indexing
-
-            smallest = index
-            if l < len(self.__heap) and self.__heap[smallest] > self.__heap[l]:
-                smallest = l
-            if r < len(self.__heap) and self.__heap[smallest] > self.__heap[r]:
-                smallest = r
-
-            if smallest != index:
-                self.__heap[index], self.__heap[smallest] = self.__heap[smallest], self.__heap[index]
-                self.__heapifyMin(smallest)
-
+    # Removes the value at root of the heap and outputs it
     def pop(self):
         if len(self.__heap) == 0:
             return None
@@ -90,28 +66,35 @@ class Heap(object):
         self.__heap.pop(len(self.__heap)-1)
 
         if len(self.__heap) != 0:
-            if self.__isMaxHeap:
-                self.__heapifyMax(0)
-            else:
-                self.__heapifyMin(0)
+            self.__heapify(0)
 
         return out
 
+    # Returns value at root without removing it
     def get_root_val(self):
         return self.__heap[0]
 
+    # Returns total number of elements in the heap
     def size(self):
         return len(self.__heap)
 
-    def printheap(self):
-        print(self.__heap)
 
+# A data structure that returns the current running median in constant time
+# Uses 2 heaps: a max heap and a min heap. The max heap holds the smaller half of the values inserted into the data
+# structure. The min heap holds the larger half of the values inserted. The 2 heaps effectively partition the set of
+# inserted values at the mid-point. The heaps are maintained in such a way that after every insertion, the size of
+# the 2 heaps differ by at most 1. The data structure returns the current median of stored elements in constant time
+# as follows:
+# If the min-heap and the max-heap have equal sizes, then the root value of the min-heap and the root value of the
+# max-heap are the 2 middle elements. The median is the average of the 2. If either heap is larger than the other
+# (by 1), the root of the larger heap is the median.
 class RunningMedianCounter(object):
     def __init__(self):
         self.__leftMaxHeap = Heap(True)
         self.__rightMinHeap = Heap(False)
         self.__total = 0      # Holds the current sum of all values in the 2 heaps
 
+    # Balances the heaps if the difference of their sizes exceeds 1
     def __balance_heaps(self):
         # If right min heap is larger than left max heap by more than 1
         if self.__rightMinHeap.size() - self.__leftMaxHeap.size() > 1:
@@ -121,8 +104,9 @@ class RunningMedianCounter(object):
             val = self.__leftMaxHeap.pop()
             self.__rightMinHeap.insert(val)
 
-        return
-
+    # Insert a value into the data structure.
+    # If both the heaps are empty, insert into the min-heap
+    #
     def insert(self, value):
         if self.__leftMaxHeap.size() == 0 and self.__rightMinHeap.size() == 0:
             self.__leftMaxHeap.insert(value)
@@ -146,6 +130,7 @@ class RunningMedianCounter(object):
         self.__balance_heaps()
         return
 
+    # Returns the running median
     def get_running_median(self):
         assert abs(self.__leftMaxHeap.size() - self.__rightMinHeap.size()) <= 1
         if self.__leftMaxHeap.size() == 0 and self.__rightMinHeap.size() == 0:
@@ -158,15 +143,14 @@ class RunningMedianCounter(object):
         else:
             return round(self.__rightMinHeap.get_root_val())
 
+    # Return the total number of elements in the 2 heaps
     def size(self):
         return self.__leftMaxHeap.size() + self.__rightMinHeap.size()
 
+    # Returns the sum of all the elements stored in the 2 heaps
     def get_current_total(self):
         return self.__total
 
-    def print_heaps(self):
-        self.__leftMaxHeap.printheap()
-        self.__rightMinHeap.printheap()
 
 
 class DataProcessor(object):
@@ -198,14 +182,6 @@ class DataProcessor(object):
         self.__date_dict[date_key].insert(transaction_amt)
         return self.__date_dict[date_key].get_running_median()
 
-    def __get_running_total_for_zip(self, zip_key):
-        assert zip_key in self.__zip_dict
-        return self.__zip_dict[zip_key].get_current_total()
-
-    def __get_transaction_count_for_zip(self, zip_key):
-        assert zip_key in self.__zip_dict
-        return self.__zip_dict[zip_key].size()
-
     # Check if date string conforms to MMDDYYYY format
     def __is_valid_date(self, date):
         try:
@@ -227,27 +203,25 @@ class DataProcessor(object):
             zip_key = cmte_id + '-' + zip
 
             running_median_for_zip = self.__insert_by_zip(zip_key, int(t_amt))
-            current_total_for_zip = self.__get_running_total_for_zip(zip_key)
-            transaction_count_for_zip = self.__get_transaction_count_for_zip(zip_key)
+            current_total_for_zip = self.__zip_dict[zip_key].get_current_total()
+            transaction_count_for_zip = self.__zip_dict[zip_key].size()
 
             by_zip_output_line += str(running_median_for_zip) + "|"
             by_zip_output_line += str(transaction_count_for_zip) + "|"
             by_zip_output_line += str(current_total_for_zip)
 
-#            print(by_zip_output_line, file=self.medians_by_zip_out)
-            print(by_zip_output_line)
+            print(by_zip_output_line, file=self.medians_by_zip_out)
 
     def __process_record_for_date(self, cmte_id, zip, t_date, t_amt):
         # Ignore record for invalid dates
         if self.__is_valid_date(t_date):
-            ## Convert date from MMDDYYYY to YYYYMMDD to enable chronological sorting
+            # Convert date from MMDDYYYY to YYYYMMDD to enable chronological sorting
             MMDD = t_date[:4]
             YYYY = t_date[4:]
             t_date = YYYY + MMDD
 
-            ## NOTE: this date_key is the cmte_id concatenated with the YYYYMMDD
-            ## so that sorting this date_key in alphabetical order automatically
-            ## sorts first by cmte_id and then chronologically by date
+            # NOTE: this date_key is the cmte_id concatenated with the YYYYMMDD so that sorting this date_key in
+            # alphabetical order automatically sorts first by cmte_id and then chronologically by date
             date_key = cmte_id + '-' + t_date
             self.__insert_by_date(date_key, int(t_amt))
 
@@ -256,7 +230,7 @@ class DataProcessor(object):
             by_date_output_line = ""
             cmte_id, date = key.split('-')
 
-            ## Convert date from YYYYMMDD to MMDDYYYY
+            # Convert date from YYYYMMDD to MMDDYYYY
             MMDD = date[4:]
             YYYY = date[:4]
             date = MMDD + YYYY
@@ -272,7 +246,7 @@ class DataProcessor(object):
             by_date_output_line += str(transaction_count_for_date) + "|"
             by_date_output_line += str(current_total_for_date)
 
-            print(by_date_output_line)
+            print(by_date_output_line, file = self.medians_by_date_out)
 
     def process(self):
         if self.__inputfile is None:
@@ -283,7 +257,13 @@ class DataProcessor(object):
         self.medians_by_zip_out = open(self.__medianvals_by_zip_file, 'w')
         self.medians_by_date_out = open(self.__medianvals_by_date_file, 'w')
 
+        counter = 0
         for line in f:
+            counter += 1
+
+            if counter % 1000 == 0:
+                print(counter)
+
             columns = line.strip().split('|')
             cmte_id = columns[self.CMTE_ID_INDEX]
             zipcode = columns[self.ZIP_CODE_INDEX][:5]
@@ -291,20 +271,21 @@ class DataProcessor(object):
             transaction_amt = columns[self.TRANSACTION_AMT_INDEX]
             other_id = columns[self.OTHER_ID_INDEX]
 
-
-            if other_id.strip() == '' and cmte_id != '' and transaction_amt != '':      # Individual contributors only and NULL checks
+            # Process records for individual contributors only and non-NULL cmte_id and transaction_amt
+            if other_id.strip() == '' and cmte_id != '' and transaction_amt != '':
                 self.__process_record_for_zip(cmte_id, zipcode, transaction_date, transaction_amt)
                 self.__process_record_for_date(cmte_id, zipcode, transaction_date, transaction_amt)
 
         self.__generate_medianvals_by_date()
+        self.medians_by_zip_out.close()
+        self.medians_by_date_out.close()
 
 
+if __name__ == "__main__":
+    # Run code
+    if len(sys.argv) != 4:
+        print("Format: python ./src/find_political_donors.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt")
+        exit(1)
 
-
-# Run code
-if len(sys.argv) != 4:
-    print("Format: python ./src/find_political_donors.py ./input/itcont.txt ./output/medianvals_by_zip.txt ./output/medianvals_by_date.txt")
-    exit(1)
-
-processor = DataProcessor(sys.argv[1], sys.argv[2], sys.argv[3])
-processor.process()
+    processor = DataProcessor(sys.argv[1], sys.argv[2], sys.argv[3])
+    processor.process()
